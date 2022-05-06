081c40f85361fd27d45a83a294afe9955f19a7eb
Daniil <daniil@MacBook-Air-Daniil.local>
2022-05-06 18:37:40 +0300

    [v0.1.0-beta.1] Tabs vs. Spaces

--- src/client.js
+++ src/client.js
@@ -1,139 +1,139 @@
 
 const { createPool,
-        format    } = require('mysql2');
+		format    } = require('mysql2');
 
 const { isPlainObject,
-        parseKeyedQuery,
-        iterableValueToArray } = require('./tools');
+		parseKeyedQuery,
+		iterableValueToArray } = require('./tools');
 
 class MySQLClient {
-    constructor (options) {
-        this._options = options;
+	constructor (options) {
+		this._options = options;
 
-        this._pool_singleton = null;
-    }
+		this._pool_singleton = null;
+	}
 
-    get _pool () {
-        if (!this._pool_singleton) {
-            this._pool_singleton = createPool(this._options);
-        }
+	get _pool () {
+		if (!this._pool_singleton) {
+			this._pool_singleton = createPool(this._options);
+		}
 
-        return this._pool_singleton;
-    }
+		return this._pool_singleton;
+	}
 
-    fork (options = {}) {
-        return new MySQLClient({
-            ...this._options,
-            ...options,
-        });
-    }
+	fork (options = {}) {
+		return new MySQLClient({
+			...this._options,
+			...options,
+		});
+	}
 
-    get database () {
-        return this._options.database;
-    }
+	get database () {
+		return this._options.database;
+	}
 
-    /* async */ query (query, values) {
-        if (isPlainObject(values)) {
-            [ query, values ] = parseKeyedQuery(query, values);
-        }
+	/* async */ query (query, values) {
+		if (isPlainObject(values)) {
+			[ query, values ] = parseKeyedQuery(query, values);
+		}
 
-        if (Array.isArray(values)) {
-            iterableValueToArray(values);
-        }
+		if (Array.isArray(values)) {
+			iterableValueToArray(values);
+		}
 
-        return new Promise((resolve, reject) => {
-            this._pool.query(
-                query,
-                values,
-                (error, rows) => {
-                    if (error) {
-                        reject(error);
-                    }
-                    else {
-                        resolve(rows);
-                    }
-                },
-            );
-        });
-    }
+		return new Promise((resolve, reject) => {
+			this._pool.query(
+				query,
+				values,
+				(error, rows) => {
+					if (error) {
+						reject(error);
+					}
+					else {
+						resolve(rows);
+					}
+				},
+			);
+		});
+	}
 
-    /* async */ execute (query, values) {
-        if (isPlainObject(values)) {
-            [ query, values ] = parseKeyedQuery(query, values);
-        }
+	/* async */ execute (query, values) {
+		if (isPlainObject(values)) {
+			[ query, values ] = parseKeyedQuery(query, values);
+		}
 
-        if (Array.isArray(values)) {
-            iterableValueToArray(values);
-        }
+		if (Array.isArray(values)) {
+			iterableValueToArray(values);
+		}
 
-        // console.log('MYSQL EXECUTE');
-        // console.log('query', query);
-        // console.log('values', values);
-        // console.log('result', format(query, values));
+		// console.log('MYSQL EXECUTE');
+		// console.log('query', query);
+		// console.log('values', values);
+		// console.log('result', format(query, values));
 
-        return new Promise((resolve, reject) => {
-            this._pool.execute(
-                query,
-                values,
-                (error, rows) => {
-                    if (error) {
-                        reject(error);
-                    }
-                    else {
-                        resolve(rows);
-                    }
-                },
-            );
-        });
-    }
+		return new Promise((resolve, reject) => {
+			this._pool.execute(
+				query,
+				values,
+				(error, rows) => {
+					if (error) {
+						reject(error);
+					}
+					else {
+						resolve(rows);
+					}
+				},
+			);
+		});
+	}
 
-    /* async */ insert ({ table, row, rows }) {
-        if (undefined === rows) {
-            if (undefined !== row) {
-                rows = [ row ];
-            }
-        }
+	/* async */ insert ({ table, row, rows }) {
+		if (undefined === rows) {
+			if (undefined !== row) {
+				rows = [ row ];
+			}
+		}
 
-        if (undefined === table) {
-            throw new Error('Argument `table` must be specified.');
-        }
-        if (undefined === rows || rows.length === 0) {
-            throw new Error('Nothing to insert.');
-        }
+		if (undefined === table) {
+			throw new Error('Argument `table` must be specified.');
+		}
+		if (undefined === rows || rows.length === 0) {
+			throw new Error('Nothing to insert.');
+		}
 
-        const fields = [];
-        const values = [];
+		const fields = [];
+		const values = [];
 
-        for (const row of rows) {
-            if (0 === fields.length) {
-                for (const [ field, value ] of Object.entries(row)) {
-                    fields.push(field);
-                    values.push(value);
-                }
-            }
-            else {
-                for (const field of fields) {
-                    values.push(
-                        row[field],
-                    );
-                }
-            }
-        }
+		for (const row of rows) {
+			if (0 === fields.length) {
+				for (const [ field, value ] of Object.entries(row)) {
+					fields.push(field);
+					values.push(value);
+				}
+			}
+			else {
+				for (const field of fields) {
+					values.push(
+						row[field],
+					);
+				}
+			}
+		}
 
-        const sql_begin = format(
-            'INSERT INTO ?? (??)',
-            [
-                table,
-                fields,
-            ],
-        );
-        const values_placeholder = new Array(rows.length).fill('(' + new Array(fields.length).fill('?').join(', ') + ')').join(', ');
+		const sql_begin = format(
+			'INSERT INTO ?? (??)',
+			[
+				table,
+				fields,
+			],
+		);
+		const values_placeholder = new Array(rows.length).fill('(' + new Array(fields.length).fill('?').join(', ') + ')').join(', ');
 
-        return this.execute(
-            `${sql_begin} VALUES ${values_placeholder};`,
-            values,
-        );
-    }
+		return this.execute(
+			`${sql_begin} VALUES ${values_placeholder};`,
+			values,
+		);
+	}
 }
 
 module.exports = MySQLClient;
--- src/tools.js
+++ src/tools.js
@@ -4,39 +4,39 @@ const isIterable = exports.isIterable = (value) => value && typeof value === 'ob
 
 const keyedQueryRegex = /:(:?)(\w+)/g;
 exports.parseKeyedQuery = (query, values) => {
-    const values_arr = values ? [] : null;
+	const values_arr = values ? [] : null;
 
-    const query_new = query.replace(
-        keyedQueryRegex,
-        (_, type_mark, key) => {
-            if (values) {
-                const value = values[key];
-                if (undefined === value) {
-                    throw new Error(`Value for key ${key} is undefined.`);
-                }
+	const query_new = query.replace(
+		keyedQueryRegex,
+		(_, type_mark, key) => {
+			if (values) {
+				const value = values[key];
+				if (undefined === value) {
+					throw new Error(`Value for key ${key} is undefined.`);
+				}
 
-                values_arr.push(value);
-            }
+				values_arr.push(value);
+			}
 
-            return (1 === type_mark.length) ? '??' : '?';
-        },
-    );
+			return (1 === type_mark.length) ? '??' : '?';
+		},
+	);
 
-    return [
-        query_new,
-        values_arr,
-    ];
+	return [
+		query_new,
+		values_arr,
+	];
 };
 
 exports.iterableValueToArray = (values) => {
-    for (const [ index, value ] of values.entries()) {
-        if (
-            isIterable(value)
-            && Array.isArray(value) !== true
-        ) {
-            values[index] = Array.from(value);
-        }
-    }
+	for (const [ index, value ] of values.entries()) {
+		if (
+			isIterable(value)
+			&& Array.isArray(value) !== true
+		) {
+			values[index] = Array.from(value);
+		}
+	}
 
-    return values;
+	return values;
 };
